b73716f54ce5dbe544954eb3e3e4c88fba7e891d
Wenjing2023 <wenjing2018uk@gmail.com>
2023-04-18 16:47:02 +0100

    1st commit

--- cypress/e2e/calculator.cy.js
+++ cypress/e2e/calculator.cy.js
@@ -1,10 +1,86 @@
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
-  it('should have working number buttons', () => {
-    cy.get('#number2').click();
-    cy.get('.display').should('contain', '2')
-  })
-})
+  it("should have working number buttons", () => {
+    cy.get("#number2").click();
+    cy.get(".display").should("contain", "2");
+  });
+  it("should have working number and total buttons", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "2");
+  });
+  it("should have working arithmetical operation and display result", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-add").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "4");
+  });
+  it("should have working multiple operations be chained together", () => {
+    cy.get("#number3").click();
+    cy.get("#operator-add").click();
+    cy.get("#number1").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "2");
+  });
+  it("should have output as expected for positive numbers", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-add").click();
+    cy.get("#number1").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "3");
+  });
+  it("should have output as expected for negative numbers", () => {
+    cy.get("#number2").click();
+    cy.get("#operator-subtract").click();
+    cy.get("#number5").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "-3");
+  });
+  it("should have output as expected for decimal numbers", () => {
+    cy.get("#number0").click();
+    cy.get("#decimal").click();
+    cy.get("#number6").click();
+    cy.get("#operator-multiply").click();
+    cy.get("#number2").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "1.2");
+  });
+  it("should have output as expected very large numbers", () => {
+    cy.get("#number1").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get("#number0").click();
+    cy.get(".display").should("contain", "1e+21");
+  });
+  it("should get infinity if divide by 0", () => {
+    cy.get("#number6").click();
+    cy.get("#operator-divide").click();
+    cy.get("#number0").click();
+    cy.get("#operator-equals").click();
+    cy.get(".display").should("contain", "Infinity");
+  });
+});
--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,18 +1,112 @@
-import React from 'react';
-import Calculator from '../containers/Calculator';
-import {render, fireEvent} from '@testing-library/react';
+import React from "react";
+import Calculator from "../containers/Calculator";
+import { render, fireEvent } from "@testing-library/react";
 
-describe('Calculator', () => {
+describe("Calculator", () => {
   let container;
   beforeEach(() => {
-    container = render(<Calculator/>)
-  })
+    container = render(<Calculator />);
+  });
 
-  it('should change running total on number enter', () => {
-    const button4 = container.getByTestId('number4');
-    const runningTotal = container.getByTestId('running-total');
+  it("should change running total on number enter", () => {
+    const button4 = container.getByTestId("number4");
+    const runningTotal = container.getByTestId("running-total");
     fireEvent.click(button4);
-    expect(runningTotal.textContent).toEqual('4');
-  })
-})
-
+    expect(runningTotal.textContent).toEqual("4");
+  });
+  it("should be able to add", () => {
+    const button1 = container.getByTestId("number1");
+    const button4 = container.getByTestId("number4");
+    const addButton = container.getByTestId("operator-add");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button1);
+    fireEvent.click(addButton);
+    fireEvent.click(button4);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("5");
+  });
+  it("should be able to subtract", () => {
+    const button7 = container.getByTestId("number7");
+    const subtractButton = container.getByTestId("operator-subtract");
+    const button4 = container.getByTestId("number4");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button7);
+    fireEvent.click(subtractButton);
+    fireEvent.click(button4);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+  it("should be able to multiple", () => {
+    const button3 = container.getByTestId("number3");
+    const multipleButton = container.getByTestId("operator-multiply");
+    const button5 = container.getByTestId("number5");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button3);
+    fireEvent.click(multipleButton);
+    fireEvent.click(button5);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("15");
+  });
+  it("should be able to devide", () => {
+    const button2 = container.getByTestId("number2");
+    const button1 = container.getByTestId("number1");
+    const divideButton = container.getByTestId("operator-divide");
+    const button7 = container.getByTestId("number7");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(divideButton);
+    fireEvent.click(button7);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("3");
+  });
+  it("should be able to concatenate", () => {
+    const button2 = container.getByTestId("number2");
+    const button1 = container.getByTestId("number1");
+    const button7 = container.getByTestId("number7");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button2);
+    fireEvent.click(button1);
+    fireEvent.click(button7);
+    expect(runningTotal.textContent).toEqual("217");
+  });
+  it("should be able to chain multiple operator", () => {
+    const button2 = container.getByTestId("number2");
+    const addButton = container.getByTestId("operator-add");
+    const button1 = container.getByTestId("number1");
+    const multipleButton = container.getByTestId("operator-multiply");
+    const button7 = container.getByTestId("number7");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button2);
+    fireEvent.click(addButton);
+    fireEvent.click(button1);
+    fireEvent.click(multipleButton);
+    fireEvent.click(button7);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("21");
+  });
+  it("should be able to chain multiple operator", () => {
+    const button2 = container.getByTestId("number2");
+    const button0 = container.getByTestId("number0");
+    const addButton = container.getByTestId("operator-add");
+    const button1 = container.getByTestId("number1");
+    const multipleButton = container.getByTestId("operator-multiply");
+    const button7 = container.getByTestId("number7");
+    const equalButton = container.getByTestId("operator-equals");
+    const runningTotal = container.getByTestId("running-total");
+    fireEvent.click(button2);
+    fireEvent.click(addButton);
+    fireEvent.click(button1);
+    fireEvent.click(equalButton);
+    fireEvent.click(button0);
+    fireEvent.click(multipleButton);
+    fireEvent.click(button7);
+    fireEvent.click(equalButton);
+    expect(runningTotal.textContent).toEqual("21");
+  });
+});
